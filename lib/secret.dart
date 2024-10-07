e89c3f06b4002e6315914e7f31744e927435b83a
Jeffery Bernasko <jefferypeter20@gmail.com>
2024-10-07 17:03:43 +0000

    added API Key

---
+++ lib/secret.dart
@@ -0,0 +1,1 @@
+const APIKEY = '7c85cdcda05d5e9b99412869d9fc6ecc';
--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -3,6 +3,8 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:weather_app/additional_info_item.dart';
 import 'package:weather_app/hourly_forecast_item.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app/secret.dart';
 
 class WeatherScreen extends StatefulWidget {
   const WeatherScreen({super.key});
@@ -12,6 +14,14 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
+  Future getCurrentWeather() async {
+    String city = 'London';
+    final res = await http.get(Uri.parse(
+        'http://api.openweathermap.org/data/2.5/weather?q=$city&APPID=$APIKEY'));
+
+    print(res.body);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
